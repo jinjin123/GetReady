a30c453ba67c04f3d66b274cd062c518556f1a9e
Jinjin123 <1293813551@qq.com>
2019-01-27 13:38:57 +0800

    fixed

--- getready/pushdata.py
+++ getready/pushdata.py
@@ -5,39 +5,35 @@ import json
 from threading import Timer
 
 
-class Saber:
-    def __init__(self):
-	pass
+def on_open(ws):
+	def HandleData():
+		os.system("rm -rf output.json;scrapy crawl quotes -o  output.json")
+		content = ''
+		f = json.load(io.open("output.json", encoding='utf-8'))
+		for time in f:
+			if time['time'].encode('utf-8').find("今日") > -1:
+				if len(time['product']) > 0:
+					for p in time['product']:
+						content += p.encode('utf-8') + '\n'
+					ws.send(content)
+		t=Timer(3,HandleData)
+		t.start()
+	HandleData()
 
-    def on_open(self,ws):
-		def HandleData():
-			os.system("rm -rf output.json;scrapy crawl quotes -o  output.json")
-			content = ''
-			f = json.load(io.open("output.json", encoding='utf-8'))
-			for time in f:
-				if time['time'].encode('utf-8').find("今日") > -1:
-					if len(time['product']) > 0:
-						for p in time['product']:
-							content += p.encode('utf-8') + '\n'
-						ws.send(content)
-			t=Timer(3,HandleData)
-			t.start()
-		HandleData()
-
-    def on_error(self,error):
+def on_error(error):
 	print error
 
-    def on_close(self,ws):
+def on_close(ws):
 	ws.close()
 		
 if __name__ == '__main__':
-    saber = Saber()
+	##debug
     websocket.enableTrace(False)
-    ws = websocket.WebSocketApp("ws://" + 'localhost' + ":5000/",
-                              on_error = saber.on_error,
-                              on_close = saber.on_close)
+    ws = websocket.WebSocketApp("ws://" + '0.0.0.0' + ":5000/",
+                              on_error = on_error,
+                              on_close = on_close)
 
-    ws.on_open = saber.on_open
+    ws.on_open = on_open
     try:
    	ws.run_forever()
     except:
