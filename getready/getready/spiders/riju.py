b43308c6e91093c2f15de080fec5c8a52e99a8e8
Jinjin123 <1293813551@qq.com>
2019-02-16 17:10:19 +0800

    update

--- getready/getready/spiders/riju.py
+++ getready/getready/spiders/riju.py
@@ -16,7 +16,7 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         base_url = 'http://zhuixinfan.com/{0}'
         for quote in response.css('table.top-list-data'):
-            if quote.css('caption::text').extract_first().encode('utf-8').find("明日") > -1:
+            if quote.css('caption::text').extract_first().encode('utf-8').find("今日") > -1:
                 if len(quote.css('td.td2').xpath('.//a[2]/@href').extract()) >0:
                     for nt in quote.css('td.td2').xpath('.//a[2]/@href').extract():
                         yield scrapy.Request(base_url.format(nt), dont_filter=True, callback=self.parse_page)
